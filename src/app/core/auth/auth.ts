d95f63fa3550264501bea38223f25af1d561f532
Arthur <arthurfelipe5567@gmail.com>
2025-11-19 17:09:52 -0300

    feat: add tailwind and login page template

--- src/app/core/auth/auth.ts
+++ src/app/core/auth/auth.ts
@@ -1,6 +1,7 @@
-import { inject, Injectable, signal } from '@angular/core'
-import { Supabase } from '../data/supabase'
-import { from } from 'rxjs'
+import { computed, inject, Injectable, signal } from '@angular/core'
+import { Supabase } from '../database/supabase'
+import { User } from '@supabase/supabase-js'
+import { UserLogin, UserSignup } from './auth.types'
 
 @Injectable({
   providedIn: 'root',
@@ -8,33 +9,60 @@ import { from } from 'rxjs'
 export class Auth {
   #supabase = inject(Supabase)
   #database = this.#supabase.client
+  #session = signal<User | null>(null)
+  session = computed(() => this.#session())
 
-  constructor() {}
-
-  get authState() {
-    return this.#database.auth.getClaims().then((res) => {
-      if (res.error || !res.data) return null
-
-      return {
-        ...res.data.claims.user_metadata,
-        email: res.data.claims.email,
-        role: res.data.claims.role,
+  constructor() {
+    this.#database.auth.onAuthStateChange((event, session) => {
+      if (event === 'INITIAL_SESSION') {
+        this.#session.set(session?.user as User)
+        return
       }
+
+      if (event === 'SIGNED_OUT') {
+        this.#session.set(null)
+      }
+    })
+  }
+
+  fetchUser() {
+    return this.#database.auth.getClaims().then(({ data, error }) => {
+      if (error || !data?.claims.user_metadata) return null
+
+      return data.claims.user_metadata
     })
   }
 
   login(loginData: UserLogin) {
     const { email, password } = loginData
 
-    const response = this.#database.auth.signInWithPassword({
-      email,
-      password,
-    })
+    return this.#database.auth
+      .signInWithPassword({
+        email,
+        password,
+      })
+      .then((res) => {
+        if (!res.error && res.data) {
+          this.#session.set(res.data.user)
+        }
 
-    return from(response)
+        return res
+      })
   }
 
-  logout() {}
+  logout() {
+    return this.#database.auth.signOut()
+  }
 
-  signup() {}
+  signup(signupData: UserSignup) {
+    const { email, password, ...metadata } = signupData
+
+    return this.#database.auth.signUp({
+      email,
+      password,
+      options: {
+        data: metadata,
+      },
+    })
+  }
 }
--- src/app/core/auth/auth.types.ts
+++ src/app/core/auth/auth.types.ts
@@ -1,8 +1,19 @@
-interface AuthState {
-  
+import { User } from '@supabase/supabase-js'
+
+export interface AuthState {
+  session: User
 }
 
-interface UserLogin {
+export interface UserLogin {
   email: string
   password: string
+}
+
+export interface UserSignup {
+  name: string
+  email: string
+  password: string
+  location: Object
+  cnpj?: string
+  cause?: string
 }
