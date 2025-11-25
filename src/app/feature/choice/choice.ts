b1f17af9f7a86cdd240c346b2a89c97cf1820e95
DaviAlencar2 <davilt.alencar@gmail.com>
2025-11-25 19:27:56 -0300

    feat: update routing links in choice, login, and ong components

--- src/app/feature/choice/choice.ts
+++ src/app/feature/choice/choice.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-choice',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './choice.html',
   styleUrl: './choice.css',
 })
