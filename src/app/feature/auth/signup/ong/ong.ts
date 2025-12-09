29985ea1e9950feb5d26436099607f7c40392d20
Arthur <arthurfelipe5567@gmail.com>
2025-12-09 10:45:56 -0300

    fix: add symbols to password validator

--- src/app/feature/auth/signup/ong/ong.ts
+++ src/app/feature/auth/signup/ong/ong.ts
@@ -48,7 +48,9 @@ export class Ong {
       [
         Validators.required,
         Validators.minLength(8),
-        Validators.pattern(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/),
+        Validators.pattern(
+          /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*()_\+\-=\{\}\[\]:"'<>,\.?\/\\|~`]).{8,}$/
+        ),
       ],
     ],
     cep: [
