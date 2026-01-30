d64a3a60e50455d574504c57d96ddd9114d726b3
marisarinho <sarinho.mariana@academico.ifpb.edu.br>
2026-01-30 10:38:19 -0300

    chore: updateconfig to enable interceptors

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,14 +1,21 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-import { provideHttpClient, withFetch } from '@angular/common/http';
+import { provideHttpClient, withFetch, withInterceptors } from '@angular/common/http';
 import { routes } from './app.routes';
-
+import { xsrfInterceptor } from './core/interceptors/xsrf/xsrf-interceptor'; 
+import { authInterceptor } from './core/interceptors/auth/auth-interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
-    provideHttpClient(withFetch())
+    provideHttpClient(
+      withFetch(), 
+      withInterceptors([ 
+        xsrfInterceptor, 
+        authInterceptor  
+      ]) 
+    )
   ]
 };
