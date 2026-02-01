c27864f09c294f391ee93a935ae6fa427781f6b2
DaviAlencar2 <davilt.alencar@gmail.com>
2026-02-01 17:10:30 -0300

    fix: problema de merge

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,12 +1,3 @@
-<<<<<<< HEAD
-import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
-import { provideHttpClient, withFetch, withInterceptors, withXsrfConfiguration } from '@angular/common/http';
-import { routes } from './app.routes';
-import { credentialsInterceptor } from '@/app/core/interceptors/credentials/credentials.interceptor';
-import { xsrfInterceptor } from '@/app/core/interceptors/xsrf/xsrf-interceptor';
-
-=======
 import {
   ApplicationConfig,
   provideBrowserGlobalErrorListeners,
@@ -22,7 +13,6 @@ import {
 import { routes } from './app.routes'
 import { credentialsInterceptor } from '@/app/core/interceptors/credentials/credentials.interceptor'
 import { xsrfInterceptor } from './core/interceptors/xsrf/xsrf-interceptor'
->>>>>>> c84448ae7ba1b8b467af7075ef66b5cd13ab2e77
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -31,11 +21,7 @@ export const appConfig: ApplicationConfig = {
     provideRouter(routes),
     provideHttpClient(
       withFetch(),
-<<<<<<< HEAD
-      withInterceptors([xsrfInterceptor, credentialsInterceptor]),
-=======
       withInterceptors([credentialsInterceptor, xsrfInterceptor]),
->>>>>>> c84448ae7ba1b8b467af7075ef66b5cd13ab2e77
       withXsrfConfiguration({
         cookieName: 'XSRF-TOKEN',
         headerName: 'X-XSRF-TOKEN',
